c83ea5946c5318c4916a41227ec98cb049d8eebf
Haruki Kondo <44923695+mashharuki@users.noreply.github.com>
2026-02-22 04:31:39 +0000

    サンプルコードを追加

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log("Hello via Bun!");
+import { Mistral } from "@mistralai/mistralai";
+
+const { MISTRAL_API_KEY } = process.env;
+
+// Mistral Client Instance
+const mistral = new Mistral({
+  apiKey: MISTRAL_API_KEY,
+});
+
+/**
+ * メイン関数
+ */
+async function run() {
+  // AIの推論呼び出し
+  const result = await mistral.chat.complete({
+    model: "mistral-large-latest",
+    messages: [
+      {
+        content:
+          "Who is the best French painter? Answer in one short sentence.",
+        role: "user",
+      },
+    ],
+    responseFormat: {
+      type: "text",
+    },
+  });
+
+  console.log(result.choices[0]?.message.content);
+}
+
+run();
